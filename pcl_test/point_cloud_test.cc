768fce8da1b7efffd2898b78aad083ea01d02178
yongchao.gan_ubuntu <yongchao.Gan@desaysv.com>
2024-07-02 18:13:38 +0800

    add pcl1.14.1 test

---
+++ pcl_test/point_cloud_test.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <pcl/common/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+int main() {
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::io::loadPCDFile("./data/milk.pcd", *cloud);
+  pcl::visualization::CloudViewer viewer("cloud viewer");
+  viewer.showCloud(cloud);
+  while (!viewer.wasStopped()) {
+  }
+
+  return 0;
+}
