9d3c71f966125183f87dabadabbfd2aa115a1387
yongchao.gan_ubuntu <yongchao.Gan@desaysv.com>
2024-07-31 18:06:27 +0800

    declaration cv_eigen_convert

--- io/transform/cv_eigen_convert.h
+++ io/transform/cv_eigen_convert.h
@@ -6,4 +6,22 @@
 #include <opencv2/core/eigen.hpp>
 #include <opencv2/opencv.hpp>
 
+template <typename T>
+std::vector<std::vector<T>> cv_mat_convert_to_vector(cv::Mat &mat, size_t rows,
+                                                     size_t cols) {}
+
+template <typename T>
+std::vector<std::vector<T>> eigen_mat_convert_to_vector(
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &mat, size_t rows,
+    size_t cols) {}
+
+template <typename T>
+cv::Mat vector_convert_to_cv_mat(std::vector<std::vector<T>> &mat, size_t rows,
+                                 size_t cols) {}
+
+template <typename T>
+Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
+vector_convert_to_eigen(std::vector<std::vector<T>> &mat, size_t rows,
+                        size_t cols) {}
+
 #endif
