52518a3698d25de3b6418e9612f2b4748fc5ee1d
yongchao.gan_ubuntu <yongchao.Gan@desaysv.com>
2024-08-30 18:41:53 +0800

    add feature_training

---
+++ loop_closure/test/feature_training.cc
@@ -0,0 +1,11 @@
+#include "DBoW3/DBoW3.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+  return 0;
+}
