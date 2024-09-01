46ae8b76ebdd748cb5c70d7478bd72f7be5a80d5
gyyc <1056616678@qq.com>
2024-09-01 23:43:36 +0800

    update loop_colsure

--- loop_closure/test/feature_training.cc
+++ loop_closure/test/feature_training.cc
@@ -1,11 +1,37 @@
 #include "DBoW3/DBoW3.h"
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
 #include <iostream>
-#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <string>
+#include <vector>
 
-int main(){
+int main() {
+  std::vector<cv::Mat> images;
+  for (int i = 1; i <= 10; i++) {
+    std::string path = "./data/data_tum/" + std::to_string(i) + ".png";
+    images.push_back(cv::imread(path, 0));
+  }
+
+  // detect ORB features
+  std::cout << "1. detecting ORB features... " << std::endl;
+  cv::Ptr<cv::Feature2D> detector = cv::ORB::create();
+  std::vector<cv::Mat> descriptors;
+  for (cv::Mat &image : images) {
+    std::vector<cv::KeyPoint> key_points;
+    cv::Mat descriptor;
+    detector->detectAndCompute(image, cv::Mat(), key_points, descriptor);
+    descriptors.push_back(descriptor);
+  }
+
+  // create vocabulary
+  //--DBoW3的简单操作
+  std::cout << "2. train dictionary based on images. creating vocabulary ... "
+            << std::endl;
+  DBoW3::Vocabulary vocab;   // 默认构造函数 k=10,d=5
+  vocab.create(descriptors); // set input
+  std::cout << "vocabulary info: " << vocab << std::endl;
+  vocab.save("vocabulary.yml.gz");
+  std::cout << "done" << std::endl;
   return 0;
 }
