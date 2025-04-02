46ea62f3d9e8c9c198ab2bbd6671d95ada51d956
yongchao.gan_ubuntu <yongchao.Gan@desaysv.com>
2025-04-02 14:05:37 +0800

    check ndt 3d part

--- lidar_3d/test/incremental_ndt_lo_test.cc
+++ lidar_3d/test/incremental_ndt_lo_test.cc
@@ -1,15 +1,12 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
-#include "direct_ndt_lo.h"
+#include "incremental_ndt_lo.h"
 #include "navigation_and_mapping/dataset_type.h"
 #include "navigation_and_mapping/io_utils.h"
 #include "ndt_3d.h"
 #include "sys_utils.h"
 
-/// 本程序以ULHK数据集为例
-/// 测试以NDT为主的Lidar Odometry
-/// 若使用PCL NDT的话，会重新建立NDT树
 DEFINE_string(bag_path, "./data/mapping_3d/test.bag", "path to rosbag");
 DEFINE_string(dataset_type, "ULHK", "NCLT/ULHK/KITTI/WXB_3D"); // 数据集类型
 DEFINE_bool(use_pcl_ndt, false, "use pcl ndt to align?");
@@ -25,13 +22,12 @@ int main(int argc, char **argv) {
   sad::RosbagIO rosbag_io(fLS::FLAGS_bag_path,
                           sad::Str2DatasetType(FLAGS_dataset_type));
 
-  sad::Direct_NDT_LO::Options options;
-  options.use_pcl_ndt_ = fLB::FLAGS_use_pcl_ndt;
-  options.ndt3d_options_.nearby_type_ = FLAGS_use_ndt_nearby_6
-                                            ? sad::NDT_3D::NearbyType::NEARBY6
-                                            : sad::NDT_3D::NearbyType::CENTER;
+  sad::IncrementalNDTLO::Options options;
+  options.ndt3d_options_.nearby_type_ =
+      FLAGS_use_ndt_nearby_6 ? sad::IncrementalNdt3D::NearbyType::NEARBY6
+                             : sad::IncrementalNdt3D::NearbyType::CENTER;
   options.display_realtime_cloud_ = FLAGS_display_map;
-  sad::Direct_NDT_LO ndt_lo(options);
+  sad::IncrementalNDTLO ndt_lo(options);
 
   rosbag_io
       .AddAutoPointCloudHandle(
@@ -49,10 +45,9 @@ int main(int argc, char **argv) {
 
   if (FLAGS_display_map) {
     // 把地图存下来
-    ndt_lo.saveMap("./data/mapping_3d/map.pcd");
+        ndt_lo.saveMap("./data/mapping_3d/incremental_ndt
+        _lo_map.pcd");
   }
-
-  LOG(INFO) << "done.";
 
   return 0;
 }
