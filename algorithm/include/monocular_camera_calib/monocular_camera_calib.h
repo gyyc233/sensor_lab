7bd21e56872f5d247849c62e8dfa02f70cf9778e
yongchao.gan_ubuntu <yongchao.Gan@desaysv.com>
2025-09-26 18:30:14 +0800

    add monocular and stereo camera notes

---
+++ algorithm/include/monocular_camera_calib/monocular_camera_calib.h
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+
+namespace Algorithm {
+class MonocularCameraCalib {
+public:
+  /// @brief 单目相机标定
+  /// @param intrinsic_filename 输出的相机内参标定结果
+  /// @param pics_path 标定图片路径
+  void SingleCalibrate(std::string intrinsic_filename = "intrinsics.yml",
+                       std::string pics_path = "single_calib_pic");
+  //将要读取的图片路径存储在fileList中
+  void InitFileList(std::string path, std::vector<std::string> &files);
+
+private:
+  const double patLen = 5.0f; // unit: mm  标定板每个格的宽度（金属标定板）
+  double imgScale = 1.0; //图像缩放的比例因子
+  cv::Size patSize = Size(6, 4);
+  cv::Size img_size;
+};
+} // namespace Algorithm
---
+++ algorithm/include/stereo_calibration/stereo_calibration.h
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <string>
+#include <vector>
+
+namespace Algorithm {
+using namespace std;
+using namespace cv;
+
+class StereoCalib {
+public:
+  void initFileList(string dir, int first, int last);
+
+  int stereoCalibrate(string intrinsic_filename, string extrinsic_filename);
+
+  /// @brief 双目立体匹配和测量
+  /// @param picNum
+  /// @param intrinsic_filename
+  /// @param extrinsic_filename
+  /// @param no_display
+  /// @param point_cloud_filename
+  /// @return
+  int stereoMatch(int picNum, string intrinsic_filename,
+                  string extrinsic_filename, bool no_display,
+                  string point_cloud_filename);
+
+private:
+  /// @brief 生成点云后保存
+  /// @param filename
+  /// @param mat
+  void saveXYZ(string filename, const Mat &mat);
+
+  /// @brief 保存视差数据
+  /// @param filename
+  /// @param mat
+  void saveDisp(const string filename, const Mat &mat);
+
+  void F_Gray2Color(Mat gray_mat, Mat &color_mat);
+
+  Mat F_mergeImg(Mat img1, Mat disp8);
+
+  Size imgSize(1280, 720);
+  Size patSize(14, 12); //每张棋盘寻找的角点个数是14*12个
+  const double patLen = 5.0f; // unit: mm  标定板每个格的宽度（金属标定板）
+  double imgScale = 1.0; //图像缩放的比例因子
+  //将要读取的图片路径存储在fileList中
+  vector<string> fileList;
+};
+} // namespace Algorithm
